0fee59601d7ffce54be59d5775c811e85fa2b853
Long Nguyen <18nguyenl@gmail.com>
2021-10-23 18:10:03 -0500

    Add datastore as dependency

--- frontend/src/loaders/express.js
+++ frontend/src/loaders/express.js
@@ -5,13 +5,15 @@ const cors = require("cors");
 const routes = require("../routes");
 
 module.exports = function (app) {
+  app.enable('trust proxy');
+
   app.use("/", express.static("src/public"));
 
   app.use(bodyParser.json());
   app.use(cors({
-    origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
   }))
 
   app.use("/add", routes.addPictures);
